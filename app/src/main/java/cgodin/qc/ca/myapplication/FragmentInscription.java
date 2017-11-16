98e1883a2a4bc8b387106a110dd537cf35161ce7
masterDAndroidBootstrap <cedricpharand@gmail.com>
2017-11-16 11:01:44 -0500

    SDSADSADAdsadfasdfdsfsaSFDFD

--- app/src/main/java/cgodin/qc/ca/myapplication/FragmentInscription.java
+++ app/src/main/java/cgodin/qc/ca/myapplication/FragmentInscription.java
@@ -24,6 +24,7 @@ public class FragmentInscription extends Fragment {
 
     public FragmentInscription() {
         // Required empty public constructor
+        //dsadsadsadsadsadsadsadsa
     }
     @Override
     public void onCreate(Bundle savedInstanceState) {
